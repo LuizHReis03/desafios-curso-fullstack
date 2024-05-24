7bdd1c44bd17756c06d4ead54a593111acd22b1a
LuizHReis03 <luizhenriquereis70@gmail.com>
2024-05-24 16:35:03 -0300

    Finalização do Site

---
+++ restaurante/script.js
@@ -0,0 +1,10 @@
+document.getElementById("botão").addEventListener("click", function() {
+    // Número de telefone para o qual você deseja enviar a mensagem
+    var phoneNumber = "5568999732168";
+    
+    // Montando o link do WhatsApp com o número de telefone e uma mensagem opcional
+    var whatsappLink = "https://api.whatsapp.com/send?phone=5568999732168";
+    
+    // Abrindo o link do WhatsApp em uma nova janela
+    window.open(whatsappLink);
+});
